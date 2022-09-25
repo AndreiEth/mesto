5f7e4bae06daf0b07e54a8996e8ec9725fc7003e
Andrei Iurchenko <iur4.andrej@yandex.ru>
2022-09-25 15:17:20 +0300

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,16 @@ const clickEditButton = document.querySelector(".profile__edit-button");
 const clickCloseButton = popUp.querySelector(".popup__close-button");
 const userName = document.querySelector(".profile__user-name")
 const userAbout = document.querySelector(".profile__user-about")
-let formElement = popUp.querySelector(".popup__form");
-let nameInput = formElement.querySelector(".popup__name");
-let jobInput = formElement.querySelector(".popup__job");
+let formElement = document.getElementById("popup-form");
+let nameInput = formElement.querySelector(".popup__text_name");
+let jobInput = formElement.querySelector(".popup__text_job");
 
 
 
 function openPopUp() {
     popUp.classList.add("popup_opened");
-    evt.preventDefault();
-    userName.textContent = nameInput.value;
-    userAbout.textContent = jobInput.value;
+    nameInput.value = userName.textContent;
+    jobInput.value = userAbout.textContent;
 }
 function closePopUp() {
     popUp.classList.remove("popup_opened");
@@ -28,3 +27,4 @@ function formSubmitHandler(evt) {
 clickEditButton.addEventListener("click", openPopUp);
 clickCloseButton.addEventListener("click", closePopUp);
 formElement.addEventListener('submit', formSubmitHandler);
+
